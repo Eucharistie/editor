f7aab620b44a56f8f16ffd4f8df45bdd36a99680
Damiaan Dufaux <damiaan@dpro.be>
2020-03-24 12:50:57 +0100

    Remove Blaze accounts config

--- imports/ui/Login/main.tsx
+++ imports/ui/Login/main.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { ReactFragment } from 'react'
 import {Box, BaseStyles, Button, Heading} from '@primer/components'
-import AccountsUIWrapper from './BlazeUIWrapper'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebook, IconDefinition, faYoutube, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import styled from 'styled-components'
 
 function loginWithFacebook() {
 	console.log("login with facebook")		
@@ -11,18 +13,37 @@ export const Login = () => (
 		<Box m={4}>
 			<Heading as="h2" fontSize={3}>Login using</Heading>
 
-			<table>
-				<tbody>
-					<tr><td><Button width="100%">YouTube</Button></td></tr>
-					<tr><td><Button width="100%" onClick={loginWithFacebook}>facebook</Button></td></tr>
-					<tr><td><Button width="100%">Tiwtter</Button></td></tr>
-				</tbody>
-			</table>
-				
-		</Box>
-
-		<Box marginLeft={4}>
-			<AccountsUIWrapper />
+			<Table>
+				<LoginButton title="YouTube" icon={faYoutube} />
+				<LoginButton title="facebook" icon={faFacebook} />
+				<LoginButton title="Twitter" icon={faTwitter} />
+			</Table>
 		</Box>
 	</BaseStyles>
+)
+
+interface LoginButtonProps {
+	title: String
+	icon: IconDefinition
+}
+
+const FullWidthButton = styled(Button)`
+	width: 100%;
+	text-align: left;
+`
+
+const LoginButton = (props: LoginButtonProps) => (
+	<tr>
+		<td>
+			<FullWidthButton>
+				<FontAwesomeIcon icon={props.icon} className="ml-4"/> {props.title}
+			</FullWidthButton>
+		</td>
+	</tr>
+)
+
+const Table = (props: {children: ReactFragment}) => (
+	<table css={``}>
+		<tbody>{props.children}</tbody>
+	</table>
 )
