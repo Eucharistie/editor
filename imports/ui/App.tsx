63384bc9277c025c46ed740e05b447c2399ddd6d
Damiaan Dufaux <damiaan@dpro.be>
2020-03-26 14:01:56 +0100

    Add base styles to dashboard

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Hello } from './Hello';
 import { Info } from './Info';
+import {BaseStyles, Flex, Box, Text} from '@primer/components'
+import {paddedContainer, containerPadding, centeredContainer} from '/imports/ui/Layout'
 
 export const Dashboard = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
+	<BaseStyles>
+		<Box {...centeredContainer} backgroundColor='#F00'>
+			<Box {...containerPadding}>
+				<Text>Top navigation</Text>
+			</Box>
+		</Box>
+
+		<Flex {...paddedContainer}>
+			<nav></nav>
+
+			<div>
+				<h1>Welcome to Meteor!</h1>
+				<Hello />
+				<Info />
+			</div>
+		</Flex>
+	</BaseStyles>
 );
